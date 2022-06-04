a9c4e81ce7f63864f9509261de8c89245e781729
Valek <ziryanovvalentin@mail.ru>
2022-06-04 12:05:24 +0300

    hashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux/es/exports'
 import store from './state/state'
 // import reportWebVitals from './reportWebVitals';
@@ -10,11 +10,11 @@ import store from './state/state'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
 

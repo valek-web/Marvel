ef4cf424b7b22985f2073e07642e6c9e49ec2475
Valek <ziryanovvalentin@mail.ru>
2022-06-09 20:11:55 +0300

    preDeploy

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './state/state'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
